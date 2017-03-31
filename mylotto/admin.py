f1f663194baa2f5281ed1f810c790d3d151835f0
SeonHyungJo <senjing9379@gmail.com>
2017-03-31 13:20:50 +0900

    Revert "first commit"
    This reverts commit 1bc071d9b4ed010145f478e67ee0ed8ff0d075bf.

--- mylotto/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from mylotto.models import GuessNumbers
-# Register your models here.
-
-admin.site.register(GuessNumbers)
--- mylotto/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MylottoConfig(AppConfig):
-    name = 'mylotto'
